e30fe21d17d4785c6e15ccc44d9e1f66d88fc22e
luizyegov2 <luiz.cassimiro@escolar.ifrn.edu.br>
2026-09-03 13:11:15 -0300

    mudanças

---
+++ app1.js
@@ -0,0 +1,9 @@
+let num = Number(prompt("Digite um número:"));
+
+if (num % 2 != 0){
+    alert(`${num} é ímpar `);
+}else if(idade < 18){
+    alert(`${num} é zero `);
+}else{
+    alert(`${num} é par `);
+}
